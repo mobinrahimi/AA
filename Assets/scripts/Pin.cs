522cd4c799b862c0896be4417d3a32a627718465
MobinRahimi <mobinrahimi0101@gmail.com>
2022-03-27 21:33:31 +0430

    convert pin to prefab

---
+++ Assets/scripts/Pin.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pin : MonoBehaviour
+{
+    public new Rigidbody2D rigidbody;
+    public float speed = 20;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rigidbody.MovePosition(rigidbody.position + Vector2.up * speed * Time.deltaTime);
+    }
+}
