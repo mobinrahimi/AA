a39b1034df2c194afd1d68ea40f15cc3f8e06808
MobinRahimi <mobinrahimi0101@gmail.com>
2022-03-27 21:09:42 +0430

    create rotate for big circle

---
+++ Assets/scripts/BigCircle.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BigCircle : MonoBehaviour
+{
+    public float speed = 100;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, 0, speed*Time.deltaTime);
+    }
+}
